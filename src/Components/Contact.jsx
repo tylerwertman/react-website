633e1b665135c0e10aa8961af9e891363d95f5d0
Tyler <tylerwertman@outlook.com>
2023-02-19 17:19:43 -0500

    contact form adj

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -6,12 +6,12 @@ const Contact = () => {
             <h2>Contact</h2>
             <div class="content" id="form"> 
                 <form action="#">
-                    <label for="fname">Name:</label>
-                    <input type="text" id="fname" name="fname" size="20" required/>
+                    <label for="fname">Name: </label>
+                    <input type="text" id="fname" name="fname" size="20" required/>&nbsp;
                     {/* <label for="lname">Last name:</label> */}
                     {/* <input type="text" id="lname" name="lname" size="20"><br/><br/> */}
                     <br class="hidden"/>
-                    <label for="email">Email:</label>
+                    <label for="email">Email: </label>
                     <input type="text" id="email" name="email" size="20" required/><br/><br/>
                     <label for="message">&nbsp;&nbsp;Message:</label><br/><br/>
                     <textarea id="message" name="message" rows="5" cols="55" required></textarea><br/><br/>
